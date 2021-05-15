1451919a1aed819114a1afaddacdafd0eedd1bcd
tconqueror <hoangnguyenduc.1101@gmail.com>
2021-05-15 11:04:15 +0700

    update

--- chodx/get_png_buf.py
+++ chodx/get_png_buf.py
@@ -8,28 +8,28 @@ def convert_image(image_name, size):
     img = Image.open(image_name).resize((size, size), 1)
     img = img.convert('L')
     image_array = np.array(img.getdata(), dtype=np.byte)
-    image_array.tofile("buffer.buf")
-    img.save("Grayscale.png")
+    image_array.tofile(".\\buffer.buf")
+    img.save(".\\Grayscale.png")
     print(img)
     return image_array
 
-def restore_image(size):
+def restore_image(path, size):
     #arr = Image.fromarray()
-    data = np.fromfile("buffer.buf", dtype = np.byte)
+    data = np.fromfile(path, dtype = np.byte)
     data = np.reshape(data, (size,size))
     print(data)
     arr = Image.fromarray(data, mode = "L")
     arr.resize(size = (size,size))
-    arr.save("extract.png")
+    arr.save(".\\extract.png")
     return arr
 
 def main():
     if (sys.argv[1] == "--extract"):
-        c = convert_image(sys.argv[2], 50) #path
+        c = convert_image(sys.argv[2], 100) #path
         print(c)
         print('Convert to buffer.buf')
     if (sys.argv[1] == "--restore"):
-        c = restore_image(50)
+        c = restore_image(sys.argv[2], 100) #path to extracted buffer
         print(c)
         print('Restore to image')
 if __name__ == "__main__":
